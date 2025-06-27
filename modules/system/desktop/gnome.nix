9f1c7835689f0d386e3387f93281c037012a49c2
SeppDev <70803492+SeppDev@users.noreply.github.com>
2025-06-27 18:44:14 +0200

    Sync

--- modules/system/desktop/gnome.nix
+++ modules/system/desktop/gnome.nix
@@ -3,15 +3,11 @@
   programs.dconf.enable = true;
 
   services = {
-    displayManager.gdm.enable = true;
-    desktopManager.gdm.enable = true;
     gnome = {
       gnome-keyring.enable = true;
     };
-    xserver = {
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
-    };
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
   };
 
 }
